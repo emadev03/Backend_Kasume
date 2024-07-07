406dd435b02aa420645a2459d909d9e9cd14220f
Krisna Aditya Pratama <krisnaadityapratama@students.amikom.ac.id>
2024-07-08 00:11:31 +0700

    Update

--- database/migrations/2024_07_07_000001_create_super_admins_table.php
+++ database/migrations/2024_07_07_000001_create_super_admins_table.php
@@ -6,11 +6,6 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateSuperAdminsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
         Schema::create('super_admins', function (Blueprint $table) {
@@ -24,11 +19,6 @@ class CreateSuperAdminsTable extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
         Schema::dropIfExists('super_admins');
--- database/migrations/2024_07_07_000002_create_users_table.php
+++ database/migrations/2024_07_07_000002_create_users_table.php
@@ -6,32 +6,23 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateUsersTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('admin_id')->nullable();
             $table->unsignedBigInteger('superadmin_id')->nullable();
-            $table->foreign('admin_id')->references('id')->on('admins')->onDelete('cascade')->onUpdate('cascade');
-            $table->foreign('superadmin_id')->references('id')->on('super_admins')->onDelete('cascade')->onUpdate('cascade');
             $table->integer('nip')->unique();
             $table->string('password');
             $table->integer('role_id')->default(2);
             $table->rememberToken();
             $table->timestamps();
+
+            $table->foreign('admin_id')->references('id')->on('admins')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('superadmin_id')->references('id')->on('super_admins')->onDelete('cascade')->onUpdate('cascade');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
         Schema::dropIfExists('users');
